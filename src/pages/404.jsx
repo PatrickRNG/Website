ea722debfdc83eae50d9e28927070500a053afa3
Patrick Passarella <patrickpassarella@hotmail.com>
2020-05-24 14:01:20 -0300

    Added a 404 redirect

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,17 +1,9 @@
-import React from 'react';
-import { Link } from 'gatsby';
+import { useEffect } from 'react';
+import { navigate } from 'gatsby';
 
-import Layout from '../components/layout/layout';
-import Head from '../components/head';
-
-const NotFound = () => {
-  return (
-    <Layout>
-      <Head title="Not found" />
-      <h1>Page not found</h1>
-      <p><Link to="/">Go to home</Link></p>
-    </Layout>
-  );
+export default () => {
+  useEffect(() => {
+    navigate('/');
+  }, []);
+  return null;
 };
-
-export default NotFound;
