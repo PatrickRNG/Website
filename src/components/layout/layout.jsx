ef0c739f63a3ff0555644f50551b1c6c39113e3d
Patrick Passarella <patrickpassarella@hotmail.com>
2020-05-24 13:46:11 -0300

    Fixed undefined variable

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -5,7 +5,7 @@ import { Container, Content, GlobalStyle } from './styles';
 const Layout = ({ children, contentStyle, location, ...rest }) => {
   return (
     <Container {...rest}>
-      <GlobalStyle smooth={location.pathname === '/'} />
+      <GlobalStyle smooth={location && location.pathname === '/'} />
       <Header />
       <Content style={contentStyle}>{children}</Content>
     </Container>
